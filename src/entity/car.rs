09567d9b43bd298cd415e363d4b5d95176b29540
HuBohy <bohyhugo@gmail.com>
2025-04-05 12:21:21 -0400

    add car attributes

--- src/entity/car.rs
+++ src/entity/car.rs
@@ -4,10 +4,12 @@ use super::super::common::car::*;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Car {
+    car_id: String,
+    client_id: String,
     brand: Brand,
+    model: Model,
     cc: Cc,
     fuel: Fuel,
-    model: Model,
     oil_quantity: OilQuantity,
     oil_type: OilType,
 }
--- src/repositoty/model/car.rs
+++ src/repositoty/model/car.rs
@@ -4,6 +4,7 @@ use surrealdb::{Datetime, RecordId};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct ModelCar {
+    id: RecordId,
     client_id: RecordId,
     brand: String,
     cc: String,
@@ -21,4 +22,41 @@ pub enum ModelFuel {
     Gasoline,
     Diesel,
     Other,
+}
+
+
+impl ModelCar {
+    pub fn id(&self) -> RecordId {
+        self.id.clone()
+    }
+    pub fn client_id(&self) -> RecordId {
+        self.client_id.clone()
+    }
+    pub fn brand(&self) -> String {
+        self.brand.clone()
+    }
+    pub fn model(&self) -> String {
+        self.model.clone()
+    }
+    pub fn cc(&self) -> String {
+        self.cc.clone()
+    }
+    pub fn oil_type(&self) -> String {
+        self.oil_type.clone()
+    }
+    pub fn oil_quantity(&self) -> String {
+        self.oil_quantity.clone()
+    }
+    pub fn year(&self) -> u32 {
+        self.year.clone()
+    }
+    pub fn fuel(&self) -> ModelFuel {
+        self.fuel.clone()
+    }
+    pub fn created_at(&self) -> Datetime {
+        self.created_at.clone()
+    }
+    pub fn updated_at(&self) -> Datetime {
+        self.updated_at.clone()
+    }
 }
