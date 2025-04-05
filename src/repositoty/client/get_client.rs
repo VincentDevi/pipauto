1f22f10c97766cd89d04f1302bdb7904c72b80a3
HuBohy <bohyhugo@gmail.com>
2025-04-05 09:35:01 -0400

    handle_get_client

--- src/repositoty/client/get_client.rs
+++ src/repositoty/client/get_client.rs
@@ -1,10 +1,15 @@
 use crate::repositoty::error::RepositoryError;
+use crate::repositoty::model::ModelClient;
 
 use super::super::super::entity::*;
 use super::super::Repository;
 
 impl Repository {
-    pub fn get_client(&self) -> Result<Client, RepositoryError> {
-        todo!()
+    pub async fn get_client(&self, id: String) -> Result<Client, RepositoryError> {
+        let query = format!("SELECT * FROM client:{};", id);
+        let mut response = self.db.query(query).await?;
+        let result: Vec<ModelClient> = response.take(0)?;
+        let client = result.get(0).cloned().ok_or(RepositoryError::DatabaseError)?;
+        Ok(client.into())
     }
 }
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,4 +1,4 @@
-use crate::handler::client::handler_fetch_clients;
+use crate::handler::client::{handler_fetch_clients, handler_get_client};
 
 use super::AppState;
 use axum::Router;
@@ -8,5 +8,6 @@ pub fn routes(app_state: AppState) -> Router {
     Router::new()
         .route("/", get(|| async { "hello Hugo ! " }))
         .route("/clients", get(handler_fetch_clients))
+        .route("/clients/{id}", get(handler_get_client))
         .with_state(app_state)
 }
