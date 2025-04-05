eef9c272de1d7e2ad7d8e08b52c55362a39a91ac
HuBohy <bohyhugo@gmail.com>
2025-04-05 09:33:04 -0400

    add id into client entity and model client

--- src/entity/client.rs
+++ src/entity/client.rs
@@ -6,6 +6,7 @@ use super::super::common::*;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Client {
+    id: String,
     name: String,
     address: Address,
     phone: Option<Phone>,
@@ -15,6 +16,7 @@ pub struct Client {
 impl From<ModelClient> for Client {
     fn from(client: ModelClient) -> Self {
         Self {
+            id: client.id().to_string(),
             name: format!("{} {}", client.first_name(), client.last_name()),
             address: Address::new(client.address()),
             phone: client.phone().map(|x| Phone::new(x)),
