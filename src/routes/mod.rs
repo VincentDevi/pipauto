e4d49c627518538bc9fd16f9478cfd3fc4e6245e
HuBohy <bohyhugo@gmail.com>
2025-04-06 05:42:35 -0400

    final final fix (for routes)

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,5 +1,6 @@
 use crate::handler::{
     client::{handler_fetch_clients, handler_get_client},
+    car::{handler_fetch_cars, handler_get_car},
     home::handler_home,
 };
 use tower_http::services::fs::ServeDir;
@@ -11,7 +12,9 @@ pub fn routes(app_state: AppState) -> Router {
     let api_router = Router::new()
         .route("/", get(handler_home))
         .route("/clients", get(handler_fetch_clients))
-        .route("/clients/{:id}", get(handler_get_client))
+        .route("/clients/{id}", get(handler_get_client))
+        .route("/cars", get(handler_fetch_cars))
+        .route("/cars/{id}", get(handler_get_car))
         .with_state(app_state);
 
     let static_router = Router::new().nest_service("/static", ServeDir::new("templates"));
