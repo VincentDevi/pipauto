b4c320146c6ed3964dfab0a039e85c83b891ae00
Vincent <vincent.devilers@gmail.com>
2025-04-07 23:56:40 +0200

    create and implement in the repository query the pagination struct

--- src/repositoty/client/fetch_clients.rs
+++ src/repositoty/client/fetch_clients.rs
@@ -1,3 +1,4 @@
+use crate::repositoty::PagingFilter;
 use crate::repositoty::error::RepositoryError;
 use crate::repositoty::model::ModelClient;
 
@@ -5,9 +6,12 @@ use super::super::super::entity::*;
 use super::super::Repository;
 
 impl Repository {
-    pub async fn fetch_clients(&self) -> Result<Vec<Client>, RepositoryError> {
-        let query = "SELECT * FROM client;";
-        let mut response= self.db.query(query).await?;
+    pub async fn fetch_clients(
+        &self,
+        paging: PagingFilter,
+    ) -> Result<Vec<Client>, RepositoryError> {
+        let query = format!("SELECT * FROM client {};", paging);
+        let mut response = self.db.query(query).await?;
         let result: Vec<ModelClient> = response.take(0)?;
         let fetched_clients = result.into_iter().map(|x| x.into()).collect();
         Ok(fetched_clients)
--- src/repositoty/mod.rs
+++ src/repositoty/mod.rs
@@ -1,12 +1,15 @@
+use std::fmt::Display;
+
+use serde::{Deserialize, Serialize};
 use surrealdb::{Surreal, engine::remote::ws::Client};
 
-mod client;
 mod car;
+mod client;
 mod error;
 pub mod model;
 
-pub use client::*;
 pub use car::*;
+pub use client::*;
 pub use error::*;
 
 #[derive(Debug, Clone)]
@@ -17,5 +20,42 @@ pub struct Repository {
 impl Repository {
     pub fn new(db: &Surreal<Client>) -> Self {
         Self { db: db.clone() }
+    }
+}
+
+#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
+pub struct PagingFilter {
+    offset: u32,
+    limit: u32,
+}
+
+impl PagingFilter {
+    pub fn new(offset: u32, limit: u32) -> Self {
+        Self { offset, limit }
+    }
+    pub fn from_path(offset: String, limit: String) -> Result<Self, RepositoryError> {
+        Ok(Self {
+            offset: offset
+                .parse::<u32>()
+                .map_err(|err| RepositoryError::ParsingError(err.to_string()))?,
+            limit: limit
+                .parse::<u32>()
+                .map_err(|err| RepositoryError::ParsingError(err.to_string()))?,
+        })
+    }
+}
+
+impl Display for PagingFilter {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "LIMIT {} START {}", self.limit, self.offset)
+    }
+}
+
+impl Default for PagingFilter {
+    fn default() -> Self {
+        Self {
+            offset: 0,
+            limit: 20,
+        }
     }
 }
