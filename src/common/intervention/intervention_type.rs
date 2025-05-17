ce43282a5578d3e550490d1ec1ed88796de1efff
Vincent <vincent.devilers@gmail.com>
2025-05-17 20:27:40 +0200

    implement serde on struct that were still untouched

--- src/common/intervention/intervention_type.rs
+++ src/common/intervention/intervention_type.rs
@@ -1,7 +1,21 @@
+use serde::{Deserialize, Serialize};
+
+use crate::repositoty::model::ModelInterventionType;
+
 use super::*;
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
 pub enum InterventionType {
     Repair,
     Maintenance(Maintenance),
+}
+
+impl TryFrom<ModelInterventionType> for InterventionType {
+    type Error = String;
+    fn try_from(value: ModelInterventionType) -> Result<Self, Self::Error> {
+        Ok(match value {
+            ModelInterventionType::Repair => Self::Repair,
+            ModelInterventionType::Maintenance(v) => Self::Maintenance(v.try_into()?),
+        })
+    }
 }
--- src/common/intervention/maintenance.rs
+++ src/common/intervention/maintenance.rs
@@ -1,4 +1,8 @@
-#[derive(Debug, Clone, Copy)]
+use serde::{Deserialize, Serialize};
+
+use crate::repositoty::model::ModelMaintenance;
+
+#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
 pub struct Maintenance {
     filter_air: bool,
     filter_cabin: bool,
@@ -6,8 +10,26 @@ pub struct Maintenance {
     type_specific_maintenance: Option<MaintenanceType>,
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
 pub enum MaintenanceType {
     FilterGasoil,
     SparkPlug,
+}
+
+impl TryFrom<ModelMaintenance> for Maintenance {
+    type Error = String;
+    fn try_from(value: ModelMaintenance) -> Result<Self, Self::Error> {
+        let type_specific_maintenance = match (value.spark_pulg(), value.filter_gasoil()) {
+            (true, false) => Ok(Some(MaintenanceType::SparkPlug)),
+            (false, true) => Ok(Some(MaintenanceType::FilterGasoil)),
+            (false, false) => Ok(None),
+            _ => Err("error in our database".to_string()),
+        }?;
+        Ok(Self {
+            filter_air: value.filter_air(),
+            filter_cabin: value.filter_cabin(),
+            filter_oil: value.filter_oil(),
+            type_specific_maintenance,
+        })
+    }
 }
